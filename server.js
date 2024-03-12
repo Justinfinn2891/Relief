621235a22070ad5fe07860013d3d4cbef98f3b8a
justinfinn2891 <jfinn5@ut.utm.edu>
2024-03-12 16:07:36 -0500

    changes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mysql = require('mysql2')
 
 const server = express()
 const port = 3000
@@ -21,16 +20,3 @@ server.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
 
-const pool = mysql.createPool({
-    host: '208.87.75.10',
-    user: 'root',
-    password: 'Crzy12123',
-    database: 'relief'
-}).promise()
-
-const result = await pool.query("SELECT *FROM relief_users")
-console.log(result)
-
-
-
-// Create connection pool
