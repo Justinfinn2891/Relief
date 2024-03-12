0b1f451543a28eb5e3d53d5a73ac333079689b07
justinfinn2891 <jfinn5@ut.utm.edu>
2024-03-12 16:00:36 -0500

    message

---
+++ database.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Crzy12123',
+    database: 'Relief'
+}).promise()
+
+const result = await pool.query("SELECT *FROM relief_users")
+console.log(result)
+
