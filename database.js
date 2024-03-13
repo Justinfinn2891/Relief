7a42be4789e2cbdb47ee45df943d007842db9301
justinfinn2891 <jfinn5@ut.utm.edu>
2024-03-13 16:01:55 -0500

    changes

--- database.js
+++ database.js
@@ -1,13 +1,52 @@
 import mysql from 'mysql2'
+ 
+// ADDS SECURITY TO POOL VARAIBLE 
+import dotenv from 'dotenv'
+dotenv.config()
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Crzy12123',
-    database: 'Relief'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
-const result = await pool.query("SELECT *FROM relief_users")
-console.log(result)
+// GET ACCESS TO ALL INFORMATION FOR THAT TABLE
+export async function getNotes(){
+    const [rows] = await pool.query("SELECT * FROM relief_users")
+    return rows
+}
 
-s
+const notes = await getNotes()
+console.log(notes)
+
+// GET ACCESS TO PARTIAL INFORMATION FROM THAT TABLE
+
+export async function getNote(User_id){
+    const[rows] = await pool.query(`
+    SELECT *
+    FROM relief_users
+    WHERE User_id = ?
+    `, [User_id])
+    return rows[0]
+}
+
+const note = await getNote(3)
+console.log(note)
+
+// ADDING INFORMATION TO A TABLE
+
+export async function createNote(Username, Password, Address, SSN, Zipcode){
+    const[result] = await pool.query(`
+    INSERT INTO relief_users(Username, Password, Address, SSN, Zipcode)
+    Values(?,?,?,?,?)
+    `, [Username, Password, Address, SSN, Zipcode])
+    return result
+}
+
+//const newNote = await createNote('Peterpan', 'A1e424255f', '844 Bearfeet DR', '552051333', '95100')
+console.log(notes)
+
+
+
+
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-
+// CHANGED IT FROM CONST EXPRESS TO IMPORT FOR CONSOLE ERRORS (CAN BE CHANGED BACK)
+import express from 'express'
 const server = express()
 const port = 3000
+
+// IMPORTING FUNCTIONS FROM DATABASE.JS FILE 
+import {getNote, getNotes, createNote} from './database.js'
 
 var indexPath = require('path')
 
@@ -14,6 +17,11 @@ server.get('/', (req, res) => {
 server.get('/survey', (req, res) => {
     res.sendFile(indexPath.resolve('views/survey.html'))
 
+})
+// EXAMPLE OF DATABASE BEING CONNECTED TO SERVER
+server.get("/notes", async (req, res) => {
+    const notes = await getNotes()
+    res.send(notes)
 })
 
 server.listen(port, () => {
