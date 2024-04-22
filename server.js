b4884d9ff532a54c29cbaa3c7ac98492db9e9276
justinfinn2891 <jfinn5@ut.utm.edu>
2024-04-22 16:35:19 -0500

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // CHANGED IT FROM CONST EXPRESS TO IMPORT FOR CONSOLE ERRORS (CAN BE CHANGED BACK)
 const express = require('express')
 const server = express()
-const port = 3001
+const port = process.env.PORT || 3000;
 
 // IMPORTING FUNCTIONS FROM DATABASE.JS FILE 
 let {getNote, getNotes, createNote, sendLogin, createLogin, createProfile, findID, findVerification, fetchUserProfile, createSurvey, findSurveyVerification, modifySurvey} = require('./database.js')
